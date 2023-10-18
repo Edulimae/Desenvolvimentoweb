a6123e2b69383bd7e96a3c993c066d6d5ce2ad74
Edulimae <edulimae@hotmail.com>
2023-10-17 22:25:54 -0300

    AulaPacoteViagem

--- Pacote_Viagem/script.js
+++ Pacote_Viagem/script.js
@@ -1,5 +1,14 @@
 function calcularDesc()
 {
-    window.alert("Calculando...");
-document.getElementById
+   let qtdPacotes= document.querySelectorAll("input[name='pacotes']").length;
+   console.log(qtdPacotes);
+   let pacotes=document.querySelectorAll("input[name='pacotes']");
+   console.log(pacotes);
+
+//             i < pacotes.length
+for (var i=0; i < qtdPacotes; i++)
+{
+    console.log(pacotes[i].value);
+
+}
 }
